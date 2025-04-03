ff5b12880b454f09078dfdcadfabdada71ee0571
lil-tammy <71616448+lil-tammy@users.noreply.github.com>
2025-04-03 16:53:24 -0700

    Add files via upload

---
+++ submission.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Xml.Schema;
+using System.Xml;
+using Newtonsoft.Json;
+using System.IO;
+using System.Runtime.Remoting.Contexts;
+
+
+
+/**
+ * This template file is created for ASU CSE445 Distributed SW Dev Assignment 4.
+ * Please do not modify or delete any existing class/variable/method names. However, you can add more variables and functions.
+ * Uploading this file directly will not pass the autograder's compilation check, resulting in a grade of 0.
+ * **/
+
+
+namespace ConsoleApp1
+{
+
+
+    public class Program
+    {
+        public static string xmlURL = "https://lil-tammy.github.io/cse445_a4/Hotels.xml";
+        public static string xmlErrorURL = "Your Error XML URL";
+        public static string xsdURL = "https://lil-tammy.github.io/cse445_a4/Hotels.xsd";
+
+        public static void Main(string[] args)
+        {
+            string result = Verification(xmlURL, xsdURL);
+            Console.WriteLine(result);
+
+
+            result = Verification(xmlErrorURL, xsdURL);
+            Console.WriteLine(result);
+
+
+            result = Xml2Json(xmlURL);
+            Console.WriteLine(result);
+        }
+
+        // Q2.1
+        public static string Verification(string xmlUrl, string xsdUrl)
+        {
+            string verificationText = "";
+
+
+            //return "No Error" if XML is valid. Otherwise, return the desired exception message.
+            return "No Error";
+        }
+
+        public static string Xml2Json(string xmlUrl)
+        {
+
+            string jsonText = "jsonText";
+            // The returned jsonText needs to be de-serializable by Newtonsoft.Json package. (JsonConvert.DeserializeXmlNode(jsonText))
+            return jsonText;
+
+        }
+    }
+
+}
